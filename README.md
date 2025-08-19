2927a75911abe3feec4db1ea91e02aae5f1c1734
mahithasri13 <srimahithasri@gmail.com>
2025-08-19 15:23:06 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # labinternal
+# wpm_internal
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET homepage */
 const index = (req, res) => {
-res.render('index', { title: 'Skillsync'});
+res.render('index', { title: 'smart_travel_planner'});
 };
 
 module.exports = {
